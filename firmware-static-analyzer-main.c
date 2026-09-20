90c40fcf801596d70d06d6b5599e69f701170f40
Elchin Guliyev <elcnqlyv24@gmail.com>
2026-09-20 11:22:24 +0400

    Implement string extraction from binary files in firmware static analyzer

--- firmware-static-analyzer-main.c
+++ firmware-static-analyzer-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 void detectSignature(unsigned char buffer[], size_t bytesRead)
 {
@@ -29,6 +30,66 @@ void detectSignature(unsigned char buffer[], size_t bytesRead)
     }
 
 }
+
+void extractStrings(FILE *file)
+{
+    char buffer[256];
+    char current[256];
+    int length = 0;
+
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0)
+    {
+        for (size_t i = 0; i < bytesRead; i++)
+        {
+            unsigned char c = buffer[i];
+            if (isprint(c))
+            {
+                if (length < 255)
+                {
+                    current[length] = c;
+                    length++;
+                }
+                
+            }
+            else
+            {
+                if (length >= 4)
+                {
+                    current[length] = '\0';
+                    printf("%s\n", current);
+                }
+                
+                length = 0;
+            }
+            
+        }
+        
+    }
+    
+    if (length >= 4)
+    {
+        current[length] = '\0';
+        printf("%s\n", current);
+    }
+    
+    //char printedText[] = {};
+    //size_t bytesRead = fread(text, 1, sizeof(text), file);
+
+    //printf("First %zu bytes:\n", bytesRead);
+
+    //for (size_t i = 0; i < bytesRead; i++)
+    //{
+    //    if (isprint((unsigned char)text[i]))
+    //    {
+    //        strcat(printedText, bytesRead);
+    //    }
+    //    
+    //}
+    //printf("Complete string is: %s\n", printedText);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -68,6 +129,11 @@ int main(int argc, char *argv[])
     detectSignature(buffer, bytesRead);
 
     printf("File size: %ld bytes\n", fileSize);
+
+    printf("\nPrintable strings:\n");
+
+    fseek(file, 0, SEEK_SET);
+    extractStrings(file);
     
     fclose(file);
 
